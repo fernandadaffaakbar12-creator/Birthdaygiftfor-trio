cf440fadfa763fa04e6823491f97b3db0cf9d054
Fernanda Daffa <emailkamu@gmail.com>
2026-08-03 11:42:31 +0700

    DONE

--- script.js
+++ script.js
@@ -175,7 +175,7 @@
         const pinPopupClose = document.getElementById('pin-popup-close');
 
         // DEFAULT PIN: Silakan ubah angka ini jika ingin PIN lain
-        const SECRET_PIN = "0000";
+        const SECRET_PIN = "0304";
 
         let pinAttempt = 0;
         let popupTimeout = null;
